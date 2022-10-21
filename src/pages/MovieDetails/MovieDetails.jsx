9337f724e5c9355d9844a75507ac39d8a3eef97b
Lavrov Andrey <Sattarlav@gmail.com>
2022-10-22 00:58:06 +0300

    add poster

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -6,16 +6,23 @@ import { getMovieDetails } from "components/Api/Api";
 export const MovieDetails = () => {
     const [movies, setMovies] = useState({});
     const [genres, setGenres] = useState([]);
+    const [poster, setPoster] = useState('');
     const {movieId} = useParams();
    
     useEffect(() => {
-        getMovieDetails(movieId).then(response => { setMovies(response); setGenres(response.genres)});
+        getMovieDetails(movieId).then(response => {
+            setMovies(response);
+            setGenres(response.genres);
+            setPoster(`https://image.tmdb.org/t/p/w500${response.poster_path}`);
+        });
     }, [movieId])
     console.log(movies);
+
+
     return (
         <>
             
-            <img src={movies} alt="" />
+            <img src={poster} alt="" />
             <h1>{movies.title} ({ movies.release_date})</h1>
             <p>User Score: {Math.round(movies.vote_average*10)} </p>
             <h2>overview</h2>
